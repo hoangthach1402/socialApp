3ce22defa55b62e570d56f48a8e301ee909d1b86
hoangthach1402 <vuhoangthach1402@gmail.com>
2022-04-20 19:28:29 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import {useState,useEffect} from 'react'
+import {getUsers,searchUser,getUser} from './graphql/query';
 import './App.css';
-
+import {useQuery} from '@apollo/client'  
 function App() {
+  const [currentPage,setCurrentPage]=useState(1);
+  const [search,setSearch] = useState();
+  const [selectedUser,setSelectedUser] = useState();
+  const {loading, error,data}= useQuery(getUsers,{variables:{
+  page:currentPage,
+  limit:10
+  }})
+ const {loading:loading_serch,error:error_search,data:data_search} = useQuery(searchUser,{
+ variables:{
+ mobile:search
+ }
+ })
+  const {loading:loading_user, error:error_user,data:data_user} = useQuery(getUser,{
+  variables:{
+  userId:selectedUser
+  }
+  })
+  const showPage = (totalPage)=>{
+    const listBtn=[]
+    for(var i=0 ; i<totalPage; i++){  <button>hello</button>  
+      listBtn.push(<button key={i} onClick={()=>setCurrentPage(i+1)}>{i+1}</button>)
+    }
+    console.log(currentPage)
+    return listBtn
+  }
+  
+ 
+  data_user && console.log(data_user)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    
+    <div className="App ">
+     <div className="row">
+      <div className="col-xs-12 col-sm-6 col-md-6 bg-info">
+      {selectedUser && <h3>hello {selectedUser}</h3>} 
+     
+     <input type="text" value={search} onInput={(e)=>setSearch(e.target.value)} placeholder='input name or mobile' />
+     <ul>
+     {data_search && search!=='' && data_search.searchUserByMobile.map(user=>{
+      return <li key={user.id} className={selectedUser===user.id?'bg-light':'dark'} onClick={()=>setSelectedUser(user.id)}>Name: {user.name} Phone: </li>
+     })}
+     
+     </ul>
+     <h3>currentpage : {currentPage}</h3>
+     <h1 >App Xin xo</h1>
+     {data && data.users.map(user=>{
+    return   <p className={selectedUser===user.id?'bg-light':''} onClick={()=>setSelectedUser(user.id)}>{user.name}</p>
+     })}
+     
+     <div>
+     <button disabled={currentPage===1}  onClick={()=>setCurrentPage(prev=>prev-1)}>prev</button>
+     {data && data.users.map((user,index)=>{
+    return <button className={currentPage===(index+1)?'bg-dark':''} onClick={()=>setCurrentPage(index)}>
+     {++index}
+     </button>
+     })
+     }
+     </div>
+     <button disabled={currentPage===10} onClick={()=>setCurrentPage(prev=>prev+1)}>next</button>
+     </div>  
+      <div className="col-xs-12 col-sm-6 col-md-6 bg-dark text-light" >
+     {data_user && <div>
+     <p>{data_user.user.name}</p>
+     </div>} 
+      </div>
+    
+      </div>
     </div>
   );
 }
---
+++ src/graphql/query.js
@@ -0,0 +1,133 @@
+import { gql } from "@apollo/client";
+
+const getProducts = gql`
+  query {
+    products {
+      id
+      name
+      type
+      price
+      stock
+      img
+    
+    }
+  }
+`;
+const getProductWithOrders =gql`
+query getProductWithOrders($productId: ID!){
+  product(id: $productId) {
+    id 
+    name 
+    price
+    stock
+    type 
+    orders {
+      createdAt
+      id
+      user{
+        name 
+      }
+      payying
+      products {
+        id 
+        productId
+        img
+        name
+        stock 
+        price
+        type 
+      }
+    } 
+  }
+}
+`
+
+const getUser = gql`
+query getUserById($userId: ID!){
+  user(id: $userId) {
+     id 
+    name 
+    address 
+    
+    orders {
+      createdAt
+      id
+      products {
+        id 
+        productId 
+        name 
+        price
+        stock 
+        img
+        }
+    payying        
+    }
+  }
+  }
+
+`;
+const getUsers = gql`
+query getUsers($page: Int!, $limit: Int!){
+  users(page: $page, limit: $limit) {
+    id
+    name
+    mobile
+  }
+}
+  
+`;
+const searchUser =gql`
+query searchUser($mobile: String!){
+  searchUserByMobile(mobile: $mobile) {
+    id
+    name
+    mobile
+  }
+}
+`
+
+const getOrders = gql`
+  query {
+    orders {
+    createdAt
+      id
+      user {
+        name
+      }
+      products {
+        id
+        productId
+        name
+        price
+      }
+      payying
+    }
+  }
+`;
+const getOrderByUser = gql`
+  query ($userId: ID!) {
+    user(id: $userId) {
+      orders {
+        createdAt
+        payying
+        products {
+          id
+          productId 
+          name
+          price
+          stock  
+        }
+      }
+    }
+  }
+`;
+
+export {
+  getUsers,
+  getUser,
+  getProducts,
+  getOrderByUser,
+  getOrders,
+  getProductWithOrders,
+  searchUser
+};
